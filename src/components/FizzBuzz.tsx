5409fa6af1db749de47ed3cd8a636396d5a29b82
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-07 09:59:07 +0100

    formatted

--- src/components/FizzBuzz.tsx
+++ src/components/FizzBuzz.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
 import { isFizzBuzz } from "../utils/isFizzBuzz";
 
-type fizzBuzzArrayType = (number|string)[];
+type fizzBuzzArrayType = (number | string)[];
 
 export default function FizzBuzz(): JSX.Element {
-const [currentValue, queueNewValue] = useState<number>(1);
-const [storedNumber, queueStoredNumber] = useState<fizzBuzzArrayType>([]);
+  const [currentValue, queueNewValue] = useState<number>(1);
+  const [storedNumber, queueStoredNumber] = useState<fizzBuzzArrayType>([]);
 
-const handleNextClick = () => {
-    queueNewValue(prevNum => prevNum + 1);
-    queueStoredNumber(prevValue =>[...prevValue, isFizzBuzz(currentValue)] );
+  const handleNextClick = () => {
+    queueNewValue((prevNum) => prevNum + 1);
+    queueStoredNumber((prevValue) => [...prevValue, isFizzBuzz(currentValue)]);
+  };
 
-    }
-
-    return (
-        <>
-          <h1>Welcome to the FizzBuzz App</h1>
-          <p>Lets Begin: {storedNumber.join(", ")}</p>
-          <button onClick={handleNextClick }>Next</button>
-        </>
-      );
-
+  return (
+    <>
+      <h1>Welcome to the FizzBuzz App</h1>
+      <p>Lets Begin: {storedNumber.join(", ")}</p>
+      <button onClick={handleNextClick}>Next</button>
+    </>
+  );
 }
--- src/utils/isFizzBuzz.ts
+++ src/utils/isFizzBuzz.ts
@@ -1,17 +1,11 @@
-export function isFizzBuzz(number:number): string|number{
-
-
-if (number % 3 === 0 && number % 5 === 0){
-    return "FizzBuzz"
-
-}else if (number % 3 === 0){
-    return "Fizz"
-
-} else if (number % 5 === 0){
-    return "Buzz"
-
-} else {
-    return number
-}
-
+export function isFizzBuzz(number: number): string | number {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
 }
